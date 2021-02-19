d939833c125ec2a495ec7e322af5a7942c407160
KebersonMoura <keberson.carvalho@gmail.com>
2021-02-19 17:40:42 -0300

    UI Chat App life-care

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,8 +4,47 @@ import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     component: HomePage,
+    children: [
+      {
+        path: 'ajuda',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../pages/ajuda/ajuda.module').then(m => m.AjudaPageModule)
+          }
+        ]
+      },
+      {
+        path: 'mensagem',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../pages/mensagem/mensagem.module').then(m => m.MensagemPageModule)
+          }
+        ]
+      },
+      {
+        path: 'online',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../pages/online/online.module').then(m => m.OnlinePageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/online',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/online',
+    pathMatch: 'full'
   }
 ];
 
@@ -13,4 +52,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HomePageRoutingModule {}
+export class HomeRoutingModule {}
